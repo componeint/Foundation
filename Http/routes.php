f46faa1573fdb2f0971259cfff3aaf6a7b80f22b
anonymoussc <50c5ac69@opayq.com>
2017-05-04 17:16:48 +0700

    Update Http/routes

--- Http/routes.php
+++ Http/routes.php
@@ -1,6 +1,5 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'foundation', 'namespace' => 'App\\Components\Foundation\Http\Controllers'], function()
-{
+Route::group(['middleware' => 'web', 'prefix' => 'foundation', 'namespace' => 'App\\Components\Foundation\Http\Controllers'], function() {
     Route::get('/', 'FoundationController@index');
 });
